f127c9939b5bbbadec9beed1b33a0f21fbe4f9cd
MarinaPolich <marina.polich@gmail.com>
2022-11-03 19:39:31 +0200

    add homework

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import { useEffect, useState } from 'react';
 import { AppBox, BoxBtn } from './App.styled';
 import * as API from '../services/api';
 import { Searchbar } from './Searchbar/Searchbar';
@@ -7,75 +7,66 @@ import { Button } from './Button/Button';
 import { Loader } from './Loader/Loader';
 import { Modal } from './Modal/Modal';
 
-export class App extends Component {
-  state = {
-    images: [],
-    qry: '',
-    page: 1,
-    isLoading: false,
-    current: null,
-  };
+export const App = () => {
+  const [images, setImages] = useState([]);
+  const [qry, setQry] = useState('');
+  const [page, setPage] = useState(1);
+  const [isLoading, setIsLoading] = useState(false);
+  const [current, setCurrent] = useState(null);
+  const [error, setError] = useState(false);
+  const [totalItems, setTotalItems] = useState(0);
 
-  setSearch = qry => {
-    this.setState({
-      qry,
-      images: [],
-      page: 1,
-      error: false,
-      totalItems: 0,
-    });
-  };
+  useEffect(() => {
+    setImages([]);
+    setPage(1);
+    setError(false);
+    setTotalItems(0);
+  }, [qry]);
 
-  async componentDidUpdate(_, prevState) {
-    const { qry, page } = this.state;
-    if (qry !== '' && (prevState.page !== page || prevState.qry !== qry)) {
-      this.setState({ isLoading: true, error: false });
-      try {
-        const response = await API.getImages(qry, page);
-        this.setState(prevState => ({
-          images: [...prevState.images, ...response.hits],
-          isLoading: false,
-          totalItems: response.totalHits,
-        }));
-      } catch (error) {
-        this.setState({ error: true });
-        console.log('error :>> ', error);
+  useEffect(() => {
+    const fetch = async () => {
+      if (qry !== '') {
+        setIsLoading(true);
+        setError(false);
+
+        try {
+          const response = await API.getImages(qry, page);
+          setImages(prevState => [...prevState, ...response.hits]);
+          setTotalItems(response.totalHits);
+          setIsLoading(false);
+        } catch (error) {
+          setError(true);
+          console.log('error :>> ', error);
+        }
       }
-    }
-  }
+    };
+    fetch();
+  }, [qry, page]);
 
-  loadMore = () =>
-    this.setState(prevState => ({
-      page: prevState.page + 1,
-    }));
+  const loadMore = () => setPage(prevState => prevState + 1);
 
-  showImg = (url, alt) => {
-    this.setState({
-      current: { url, alt },
-    });
+  const showImg = (url, alt) => {
+    setCurrent({ url, alt });
   };
 
-  closeImg = () => {
-    this.setState({ current: null });
+  const closeImg = () => {
+    setCurrent(null);
   };
 
-  render() {
-    const { images, isLoading, current, totalItems, error } = this.state;
-    return (
-      <AppBox>
-        <Searchbar onSubmit={this.setSearch} />
-        {error && <p>Sorry... Page Not Found</p>}
-        <ImageGallery images={images} showImg={this.showImg} />
-        <BoxBtn>
-          {isLoading && <Loader />}
-          {images.length > 0 && images.length < totalItems && (
-            <Button onClick={this.loadMore} />
-          )}
-        </BoxBtn>
-        {!!current && (
-          <Modal src={current.url} alt={current.alt} onClose={this.closeImg} />
+  return (
+    <AppBox>
+      <Searchbar onSubmit={setQry} />
+      {error && <p>Sorry... Page Not Found</p>}
+      <ImageGallery images={images} showImg={showImg} />
+      <BoxBtn>
+        {isLoading && <Loader />}
+        {images.length > 0 && images.length < totalItems && (
+          <Button onClick={loadMore} />
         )}
-      </AppBox>
-    );
-  }
-}
+      </BoxBtn>
+      {!!current && (
+        <Modal src={current.url} alt={current.alt} onClose={closeImg} />
+      )}
+    </AppBox>
+  );
+};
--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,42 +1,64 @@
-import React, { Component } from 'react';
+import { useEffect } from 'react';
 import { createPortal } from 'react-dom';
 import PropTypes from 'prop-types';
 import { ModalBox, Overlay } from './Modal.styled';
 
 const modalRoot = document.querySelector('#modal-root');
-export class Modal extends Component {
-  componentDidMount() {
-    window.addEventListener('keydown', this.handleKeyDown);
-  }
 
-  componentWillUnmount() {
-    window.removeEventListener('keydown', this.handleKeyDown);
-  }
+export const Modal = ({ src, alt, onClose }) => {
+  useEffect(() => {
+    const handleKeyDown = ({ code }) => code === 'Escape' && onClose();
+    window.addEventListener('keydown', handleKeyDown);
+    return () => {
+      window.removeEventListener('keydown', handleKeyDown);
+    };
+  }, [onClose]);
 
-  handleKeyDown = e => {
-    if (e.code === 'Escape') {
-      this.props.onClose();
-    }
-  };
+  const handleClick = ({ currentTarget, target }) =>
+    currentTarget === target && onClose();
 
-  handleClick = e => {
-    if (e.currentTarget === e.target) {
-      this.props.onClose();
-    }
-  };
+  return createPortal(
+    <Overlay onClick={handleClick}>
+      <ModalBox>
+        <img src={src} alt={alt} />
+      </ModalBox>
+    </Overlay>,
+    modalRoot
+  );
+};
+// export class ModalOld extends Component {
+//   componentDidMount() {
+//     window.addEventListener('keydown', this.handleKeyDown);
+//   }
 
-  render() {
-    const { src, alt } = this.props;
-    return createPortal(
-      <Overlay onClick={this.handleClick}>
-        <ModalBox>
-          <img src={src} alt={alt} />
-        </ModalBox>
-      </Overlay>,
-      modalRoot
-    );
-  }
-}
+//   componentWillUnmount() {
+//     window.removeEventListener('keydown', this.handleKeyDown);
+//   }
+
+//   handleKeyDown = e => {
+//     if (e.code === 'Escape') {
+//       this.props.onClose();
+//     }
+//   };
+
+//   handleClick = e => {
+//     if (e.currentTarget === e.target) {
+//       this.props.onClose();
+//     }
+//   };
+
+//   render() {
+//     const { src, alt } = this.props;
+//     return createPortal(
+//       <Overlay onClick={this.handleClick}>
+//         <ModalBox>
+//           <img src={src} alt={alt} />
+//         </ModalBox>
+//       </Overlay>,
+//       modalRoot
+//     );
+//   }
+// }
 Modal.propTypes = {
   onClose: PropTypes.func.isRequired,
   src: PropTypes.string.isRequired,
